f79915a1f913957ff3ba333f4e3c4f19b30c471e
armintalaie <talaiearmin78@gmail.com>
2021-01-19 19:14:22 -0800

    heroku update

--- MovieRecApp/urls.py
+++ MovieRecApp/urls.py
@@ -21,6 +21,7 @@ from intro.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('intro/', intro),
+    path('/', intro),
     path('external/', external),
     path('add_movie/', add_movie),
     path('results/', give_results),
